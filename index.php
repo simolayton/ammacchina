fce1ab758d9a160f5776feee7703226b5cda9d0a
simolayton <smameli13@gmail.com>
2014-12-09 16:37:32 +0100

    Messaggio per la commit

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>AMMacchina</title>
+	</head>
+	
+    <body>
+	
+	<header>
+        <div style="text-align: center">
+            <h1><img src="1500.png" width="600" height="250" />
+        </div>
+    </header>
+	
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	
+	<div style="text-align: center">
+	
+		<form method="post" action="php/login.php">
+		
+			<input type="hidden" name="cmd" value="login"/>
+			<label for="user">Username</label>
+			<input type="text" name="username" id="username"/>
+			<br>
+			<br>
+			<label for="password">Password </label>
+			<input type="password" name="password" id="password"/> 
+			<br>
+			<br>
+			<input type="submit" value="Login"/>
+		
+		</form>
+	</div>
+	</body>
+	
+</html>
