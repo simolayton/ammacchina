4434fa96e19a7ff78f457a3ab69467d7e4a6c935
simolayton <smameli13@gmail.com>
2014-12-10 10:33:00 +0100

    Update db_con.php

--- php/db_con.php
+++ php/db_con.php
@@ -1,19 +1,19 @@
 <?php
 
 //connessione al nostro database
-$connessione_al_server=mysql_connect("amm14_mameliSimone","mameliSimone","macaco861");
+$connessione_al_server=mysql_connect("users","mameliSimone","macaco861");
 // ip locale, login e password
 if(!$connessione_al_server)
 {
-	// questo apparirà solo se ci sarà un errore
+	// questo apparirÃ  solo se ci sarÃ  un errore
 	die ('Non riesco a connettermi: errore '.mysql_error());
 }
 
 $db_selected=mysql_select_db("users",$connessione_al_server);
-// dove io ho scritto "prova" andrà inserito il nome del db
+// dove io ho scritto "prova" andrÃ  inserito il nome del db
 if(!$db_selected)
 {
-	// se la connessione non andrà a buon fine apparirà questo messaggio
+	// se la connessione non andrÃ  a buon fine apparirÃ  questo messaggio
 	die ('Errore nella selezione del database: errore '.mysql_error()); 
 }
 
