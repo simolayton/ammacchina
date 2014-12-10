f48574a7bfbca4696de0da271dbf4541e9de6b9c
simolayton <smameli13@gmail.com>
2014-12-10 10:27:04 +0100

    Update login.php

--- php/login.php
+++ php/login.php
@@ -4,9 +4,9 @@
 session_start();
  // Include il file di connessione al database
 include("db_con.php");
-// con questo associo il parametro username che mi è stato passato dal form alla variabile SESSION username
+// con questo associo il parametro username che mi Ã¨ stato passato dal form alla variabile SESSION username
 $_SESSION["username"]=$_POST["username"]; 
-// con questo associo il parametro username che mi è stato passato dal form alla variabile SESSION password
+// con questo associo il parametro username che mi Ã¨ stato passato dal form alla variabile SESSION password
 $_SESSION["password"]=$_POST["password"]; 
 //per selezionare nel db l'utente e pw che abbiamo appena scritto nel log
 
@@ -17,17 +17,17 @@ $query = mysql_query("SELECT * FROM users WHERE username='".$_POST["username"]."
 //	header("location:php/prova.php"); 
 //}
 	
-// Con il SELECT qua sopra selezione dalla tabella users l utente registrato (se lo è) con i parametri che mi ha passato il form di login, quindi
+// Con il SELECT qua sopra selezione dalla tabella users l utente registrato (se lo Ã¨) con i parametri che mi ha passato il form di login, quindi
 // Quelli dentro la variabile POST. username e password.
 
-//se c'è una persona con quel nome nel db allora loggati
-if(mysql_num_rows($query)&gt;0)
+//se c'Ã¨ una persona con quel nome nel db allora loggati
+if(mysql_num_rows($query))//&gt;0)
 {   
 	// metto i risultati dentro una variabile di nome $row
 	$row = mysql_fetch_assoc($query); 
 	// Nella variabile SESSION associo TRUE al valore logged
 	$_SESSION["logged"] =true;  
-	// e mando per esempio ad una pagina esempio.php// in questo caso rimanderò ad una pagina prova.php
+	// e mando per esempio ad una pagina esempio.php// in questo caso rimanderÃ² ad una pagina prova.php
 	header("location:php/prova.php"); 
 }
 else
