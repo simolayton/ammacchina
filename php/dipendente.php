4dbac6a012385b175784655d75e13b4cf716b6c0
simolayton <smameli13@gmail.com>
2014-12-12 15:54:35 +0100

    Update dipendente.php

---
+++ php/dipendente.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+
+        <head>
+          <title> AMMacchina </title>
+        </head>
+        
+<body>
+        <div style="text-align: center"> 
+        
+              <h3>Sei sulla pagina del DIPENDENTE</h3>
+
+</body>
+
+</html>
