fa3719632357b02d6afaf90dd5af6f207a53701d
simolayton <smameli13@gmail.com>
2015-01-04 22:04:31 +0100

    Update cliente.php

--- php/cliente.php
+++ php/cliente.php
@@ -2,16 +2,63 @@
 <html>
 
         <head>
-                <title> AMMacchina </title>
-                <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-                <link rel="stylesheet" type="text/css" href="../css/style.css">
-                <link rel="shortcut icon" href="../img/icona.ico">
+                <title>AMMacchina</title>
+        	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        	<link rel="stylesheet" type="text/css" href="../css/style.css">
+        	<link rel="shortcut icon" href="../img/icona.ico">
         </head>
         
-<body>
-        <div style="text-align: center"> 
+	<body>
+	
+        <div id="page">
+                
+                <header>
+                
+                    <div style="text-align: center" id="header">
+                        <img src="../img/1500.png" alt="" width="600" height="250"/>
+                    </div>
+                    
+                    <div style="text-align: center" id="top">
+                        <p>Benvenuto! ZONA CLIENTE</p>
+                    </div>
+                    
+                </header>
+		
+		<!--
+			<div style="text-align: center" div id="sidebar1">
+				<h2>Navigazione</h2>
+				<ul>
+					<li><a href="index.php">Home</a></li>
+				</ul>
+                </div>-->
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+                
+        <div id="footer">
         
-              <h3>Sei sulla pagina del CLIENTE</h3>
+                <div style="text-align: center">
+                        <b><i>Simone Mameli</i></b>
+                </div>
+                
+                <br>
+                <br>
+                
+                <div style="text-align: center">
+                        <a id="htmlval" href="http://validator.w3.org/check?uri=referer">HTML Valid</a>
+
+                        <a id="cssval" href="http://jigsaw.w3.org/css-validator/check/refer">CSS Valid</a>
+                </div>
+        </div>
+        </div>
 
 </body>
 
