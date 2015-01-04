3d8fcc5f8714600f9b44219e36134d44c5668233
simolayton <smameli13@gmail.com>
2015-01-04 18:08:56 +0100

    Update dipendente.php

--- php/dipendente.php
+++ php/dipendente.php
@@ -4,8 +4,8 @@
         <head>
                 <title>AMMacchina</title>
         	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        	<link rel="stylesheet" type="text/css" href="../../css/style.css">
-        	<link rel="shortcut icon" href="../../img/icona.ico">
+        	<link rel="stylesheet" type="text/css" href="../css/style.css">
+        	<link rel="shortcut icon" href="../img/icona.ico">
         </head>
         
 <body>
