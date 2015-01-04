e0616882851a5f0a9c17a606f0e43e53397d2d07
simolayton <smameli13@gmail.com>
2015-01-04 21:45:52 +0100

    Update dipendente.php

--- php/dipendente.php
+++ php/dipendente.php
@@ -8,73 +8,56 @@
         	<link rel="shortcut icon" href="../img/icona.ico">
         </head>
         
-<body>
-        <div style="text-align: center"> 
-        
-              <h3>Sei sulla pagina del DIPENDENTE</h3>
-              
-              
-                    <div id="page">
+	<body>
+	
+        <div id="page">
+                
                 <header>
+                
                     <div style="text-align: center" id="header">
                         <img src="../img/1500.png" alt="" width="600" height="250"/> <!-- <font face="Times New Roman">AMMacchina</font></h1> -->
                     </div>
+                    
                     <div style="text-align: center" div id="top">
-                        <p>Accesso al sistema</p>
+                        <p>Benvenuto! ZONA DIPENDENTE</p>
                     </div>
+                    
                 </header>
-				<!--
-				<div style="text-align: center" div id="sidebar1">
-						<h2>Navigazione</h2>
-					<ul>
-						<li><a href="index.php">Home</a></li>
-					</ul>
+		
+		<!--
+			<div style="text-align: center" div id="sidebar1">
+				<h2>Navigazione</h2>
+				<ul>
+					<li><a href="index.php">Home</a></li>
+				</ul>
                 </div>-->
 				<br>
 				<br>
 				<br>
-					<div style="text-align: center" div id="content">
+	<div style="text-align: center" div id="content">
 	
-					<div class="input-form">
-					<h3>Login</h3>
-	
-			<form method="post" action="../php/login.php">
-		
-				<input type="hidden" name="cmd" value="login"/>
-				<label for="user">Username</label>
-				<input type="text" name="username" id="username"/>
-				<br>
-				<br>
-				<label for="password">Password</label>
-				<input type="password" name="password" id="password"/> 
-				<br>
-				<br>
-				<input type="submit" value="Login"/>
-	
-			</form>
+		<div class="input-form">
+				<h3>Login</h3>
 		</div>
 	</div>
 				
-				
-				<br>
-				<br>
-				<br>
-				<br>
-                <div id="clear">
-
-                </div>
-
-                <div id="footer">
-                    <p align="center">
+	<br>
+	<br>
+	<br>
+	<br>
+                
+        <div id="footer">
+        
+                <p align="center">
                         <b><i>Simone Mameli</i></b>
                     </p>
-                    <p align="center">
+                <p align="center">
                         <a id="htmlval" href="http://validator.w3.org/check?uri=referer">HTML Valid</a>
 
                         <a id="cssval" href="http://jigsaw.w3.org/css-validator/check/refer">CSS Valid</a>
-                    </p>
-                </div>
-            </div>
+                </p>
+        </div>
+        </div>
 
 </body>
 
