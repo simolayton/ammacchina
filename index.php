00486523f439d128d2020adf8877c08ddd28216d
simolayton <smameli13@gmail.com>
2015-01-04 17:45:05 +0100

    Update index.php

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 	
 	<header>
         	<div style="text-align: center">
-            		<img src="img/1500.png" width="600" height="250"/>
+            		<img src="img/1500.png" alt="" width="600" height="250"/>
         	</div>
 	 </header>
 	
