dcf9370d08e09dd683232f19f5cfd954f4c57d7d
simolayton <smameli13@gmail.com>
2015-01-04 16:51:38 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,8 +2,10 @@
 <html>
     	<head>
         	<title>AMMacchina</title>
+        	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        	<link rel="stylesheet" type="text/css" href="css/style.css">
         	<link rel="shortcut icon" href="img/icona.ico">
-        	<style>
+        	<!-- <style>
         	
 			.button
 			{
@@ -20,7 +22,7 @@
 			display: block;
 			}
 			
-		</style>
+		</style>-->
 	</head>
 	
 	<br>
