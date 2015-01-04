d4c8929b2e0393920213b15eb858348b12cf4b58
simolayton <smameli13@gmail.com>
2015-01-04 17:16:44 +0100

    Update login.php

--- php/login.php
+++ php/login.php
@@ -51,8 +51,8 @@ else
     	<head>
         	<title>AMMacchina</title>
         	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        	<link rel="stylesheet" type="text/css" href="css/style.css">
-        	<link rel="shortcut icon" href="img/icona.ico">
+        	<link rel="stylesheet" type="text/css" href="../css/style.css">
+        	<link rel="shortcut icon" href="../img/icona.ico">
 	</head>
 	
 	<br>
