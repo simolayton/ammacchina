5eda39f6b041d3cdb78fef5396afe12ebba1f6cf
simolayton <smameli13@gmail.com>
2015-01-05 15:20:32 +0100

    logout

---
+++ php/logout.php
@@ -0,0 +1,15 @@
+<?php
+if(!isset($_COOKIE["redirect"]))
+{
+    $pagina_login = "login.php";
+}
+else
+{
+    $pagina_login = $_COOKIE["redirect"];
+}
+setcookie("tipo_utente", null);
+setcookie("id_utente", null);
+setcookie("logout", 1);
+session_destroy();
+header("Location:".$pagina_login);
+?>
