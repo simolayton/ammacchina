59899dae9446f519eb5180613e7280cdb5ec021a
simolayton <smameli13@gmail.com>
2015-01-05 16:27:55 +0100

    Update dipendente.php

--- php/dipendente.php
+++ php/dipendente.php
@@ -1,11 +1,5 @@
 <!DOCTYPE html>
 
-<?php
-	setcookie("redirect", null);
-	if($_COOKIE['tipo_utente']==2)
-{
-?>
-
 <html>
 
         <head>
@@ -70,14 +64,3 @@
 </body>
 
 </html>
-
-<?
-}
-else
-{
-    $pagina_login = "../index.php";
-    $pagina_redirect = "dipendete.php";
-    setcookie("redirect", $pagina_redirect, time()+300);
-	header("Location:".$pagina_login);
-}
-?>
