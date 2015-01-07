f3580b004caa660de96573984b919e71dc79d9e6
simolayton <smameli13@gmail.com>
2015-01-07 19:00:38 +0100

    Update parcoauto.php

--- php/dipendente/parcoauto.php
+++ php/dipendente/parcoauto.php
@@ -34,7 +34,7 @@
                         <li class="current_page"><a href="#" id="parcoauto">Parco Auto</a></li>
                         <li><a href="aggiungiauto.php" id="aggiungiauto">Aggiungi Auto</a></li>
                         <li><a href="modificaauto.php" id="modificaauto">Modifica Auto</a></li>
-                        <li><a href="../php/logout.php" id="logout">Logout</a></li>
+                        <li><a href="../../php/logout.php" id="logout">Logout</a></li>
                     </ul>
                 </div>
 		</div>
