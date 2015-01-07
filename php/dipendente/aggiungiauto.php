4455e1d5c84274bc5559c7d381267018b45066c5
simolayton <smameli13@gmail.com>
2015-01-07 19:01:19 +0100

    Update aggiungiauto.php

--- php/dipendente/aggiungiauto.php
+++ php/dipendente/aggiungiauto.php
@@ -33,7 +33,7 @@
                         <li><a href="parcoauto.php" id="parcoauto">Parco Auto</a></li>
                         <li class="current_page"><a href="#" id="aggiungiauto">Aggiungi Auto</a></li>
                         <li><a href="modificaauto.php" id="modificaauto">Modifica Auto</a></li>
-                        <li><a href="../php/logout.php" id="logout">Logout</a></li>
+                        <li><a href="../../php/logout.php" id="logout">Logout</a></li>
                     </ul>
                 </div>
 		</div>
