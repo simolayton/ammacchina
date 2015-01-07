87a1f500108e9288f9f1638665c0e4c5a9c4fdf1
simolayton <smameli13@gmail.com>
2015-01-07 18:59:48 +0100

    Update modificaauto.php

--- php/dipendente/modificaauto.php
+++ php/dipendente/modificaauto.php
@@ -32,8 +32,8 @@
                     <ul>
                         <li><a href="home.php" id="home">Home</a></li>
                         <li><a href="parcoauto.php" id="parcoauto">Parco Auto</a></li>
-                        <li><a href="aggiungi.php" id="aggiungi">Aggiungi Auto</a></li>
-                        <li class="current_page"><a href="#" id="modifica">Modifica Auto</a></li>
+                        <li><a href="aggiungiauto.php" id="aggiungiauto">Aggiungi Auto</a></li>
+                        <li class="current_page"><a href="#" id="modificaauto">Modifica Auto</a></li>
                         <li><a href="../php/logout.php" id="logout">Logout</a></li>
                     </ul>
                 </div>
