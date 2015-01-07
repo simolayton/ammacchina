84935968793b79126fd2dccc8155f65a21e3e968
simolayton <smameli13@gmail.com>
2015-01-07 18:49:36 +0100

    Update parcoauto.php

---
+++ php/dipendente/parcoauto.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+
+<html>
+
+        <head>
+        
+            	<script language="javascript">
+			if(history.length>0)history.forward()
+		</script>
+        
+                <title>AMMacchina</title>
+        	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        	<link rel="stylesheet" type="text/css" href="../css/style.css">
+        	<link rel="shortcut icon" href="../img/icona.ico">
+        </head>
+        
+	<body>
+	
+        <div id="page">
+                    
+                <header>
+                
+                    <div style="text-align: center" id="header">
+                        <img src="../img/1500.png" alt="" width="600" height="250"/>
+                    </div>
+                    
+                    <div style="text-align: center" id="top">
+                    	<!-- <button id="button" type="submit" name="cmd"  value="home">Home</button> -->
+		    
+
+		<div style="text-align: center" id="menu">
+                    <ul>
+                        <li><a href="dipendente/home.php" id="home">Home</a></li>
+                        <li class="current_page"><a href="#" id="parcoauto">Parco Auto</a></li>
+                        <li><a href="dipendente/aggiungi.php" id="aggiungi">Aggiungi Auto</a></li>
+                        <li><a href="dipendente/modifica.php" id="modifica">Modifica Auto</a></li>
+                        <li><a href="../php/logout.php" id="logout">Logout</a></li>
+                    </ul>
+                </div>
+		</div>
+		    
+		    
+		    
+                </header>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+                
+        <div id="footer">
+        
+                <div style="text-align: center">
+                        <b><i>Simone Mameli</i></b>
+                </div>
+                
+                <br>
+                <br>
+                
+                <div style="text-align: center">
+                        <a id="htmlval" href="http://validator.w3.org/check?uri=referer" target="_blank">HTML Valid</a>
+
+                        <a id="cssval" href="http://jigsaw.w3.org/css-validator/check/refer" target="_blank">CSS Valid</a>
+                </div>
+        </div>
+        </div>
+
+</body>
+
+</html>
