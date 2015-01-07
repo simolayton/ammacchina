a81a17ef0e75f544034e3d7a9bfdee3cee812d9d
simolayton <smameli13@gmail.com>
2015-01-07 16:59:46 +0100

    Update logout.php

--- php/logout.php
+++ php/logout.php
@@ -1,15 +1,20 @@
-<?php
-if(!isset($_COOKIE["redirect"]))
-{
-    $pagina_login = "../index.php";
-}
-else
-{
-    $pagina_login = $_COOKIE["redirect"];
-}
-setcookie("tipo_utente", null);
-setcookie("id_utente", null);
-setcookie("logout", 1);
-session_destroy();
-header("Location:".$pagina_login);
-?>
+<!DOCTYPE html>
+
+<html>
+    	<head>
+    		<script language="javascript">
+			if(history.length>0)history.forward()
+		</script>
+		
+		</head>
+		
+		<body>
+		</body>
+		
+		</html>
+
+
+
+
+
+
