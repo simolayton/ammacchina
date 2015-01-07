f97a57f39708149c256ffea3fb237320aaa28963
simolayton <smameli13@gmail.com>
2015-01-07 16:42:18 +0100

    Update parcoauto.php

--- php/cliente/parcoauto.php
+++ php/cliente/parcoauto.php
@@ -5,7 +5,7 @@
         <head>
                 <title>AMMacchina</title>
         	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        	<link rel="stylesheet" type="text/css" href="../css/style.css">
+        	<link rel="stylesheet" type="text/css" href="../../css/style.css">
         	<link rel="shortcut icon" href="../../img/icona.ico">
         </head>
         
