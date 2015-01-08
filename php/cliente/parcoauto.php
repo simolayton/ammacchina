5dd3ae167716af1fe840a9a5e95619ea04404c36
simolayton <smameli13@gmail.com>
2015-01-08 11:30:09 +0100

    Update parcoauto.php

--- php/cliente/parcoauto.php
+++ php/cliente/parcoauto.php
@@ -43,13 +43,13 @@
                 </header>
                 
                 <?php
-                $connessione_al_server = mysql_connect("localhost","truduGabriele","beluga874");
+                $connessione_al_server = mysql_connect("localhost","mameliSimone","macaco861");
                 
                 if(!$connessione_al_server)
                 {
                 	die("Errore: connessione non riuscita".mysql_error());
             	}
-            	$db_selected = mysql_select_db("amm14_truduGabriele", $connessione_al_server);
+            	$db_selected = mysql_select_db("amm14_mameliSimone", $connessione_al_server);
             	if(!$db_selected)
             	{
                 	die("Errore: selezione del database errata ".mysql_error());
