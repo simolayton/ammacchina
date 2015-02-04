60fabfe458310828c1f5daa6de80baa9834f4578
simolayton <smameli13@gmail.com>
2015-02-04 18:26:12 +0100

    Update parcoauto.php

--- php/cliente/parcoauto.php
+++ php/cliente/parcoauto.php
@@ -32,7 +32,7 @@
                         <li><a href="home.php" id="home">Home</a></li>
                         <li class="current_page"><a href="#" id="parcoauto">Parco Auto</a></li>
                         <li><a href="carrello.php" id="carrello">Carrello</a></li>
-                        <li><a href="contatti.php" id="contatti">Contatti</a></li>
+                        <li><a href="ricerca.php" id="ricerca">Ricerca</a></li>
                         <li><a href="../../php/logout.php" id="logout">Logout</a></li>
                     </ul>
                 </div>
