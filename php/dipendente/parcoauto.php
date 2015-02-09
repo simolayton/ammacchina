77ac6d508628234fdfc92cfb7b9b13513f16d858
simolayton <smameli13@gmail.com>
2015-02-09 18:40:25 +0100

    Update parcoauto.php

--- php/dipendente/parcoauto.php
+++ php/dipendente/parcoauto.php
@@ -42,15 +42,99 @@
 		    
 		    
                 </header>
-	<br>
-	<br>
-	<br>
-	<br>
-	<br>
-	<br>
-	<br>
-	<br>
-	<br>
+                
+                <?php
+                $connessione_al_server = mysql_connect("localhost","mameliSimone","macaco861");
+                
+                if(!$connessione_al_server)
+                {
+                	die("Errore: connessione non riuscita".mysql_error());
+            	}
+            	$db_selected = mysql_select_db("amm14_mameliSimone", $connessione_al_server);
+            	if(!$db_selected)
+            	{
+                	die("Errore: selezione del database errata ".mysql_error());
+            	}
+            	$queryvis = mysql_query("SELECT * FROM cars WHERE stato='Auto usata'") or die("query non riuscita".mysql_error());
+            	$row = mysql_fetch_object($queryvis);
+                ?>
+                
+		
+			
+                
+                
+            <?php
+            while($row = mysql_fetch_object($queryvis))
+            {
+            ?>
+            
+            
+
+         
+             <br>
+
+                   
+                   <div style="text-align: center">
+                   
+                 	<img src="../../img/pauto.png" width="100" height="70" alt="">
+                   
+                        <?echo"$row->marca";?>
+                        
+                        <?echo"$row->modello";?>
+                        
+			<?echo"$row->anno";?>
+			
+                        <?echo"$row->colore";?>
+                        
+                        <?echo"$row->alimentazione";?>
+
+                        <?echo"$row->chilometri";?>
+                        
+                        <b>PREZZO : </b><?echo"$row->prezzo";?>
+                        
+                        <a href="parcoauto.php?aggiungi=<?echo $row->id?>" id="button">Aggiungi al carrello</a></td>
+                        
+                        <!-- <input type="button" id="button" onclick="'https://www.paypal.com/it/home'" value="COMPRA"> -->
+                        
+                 </div> 
+              <?php
+                            }
+                            ?>
+                            <?php
+                            if(isset($_GET["aggiungi"]) && ($_GET["aggiungi"]!=0))
+                            {
+                                $idauto = $_GET["aggiungi"];
+				$querypresagg = mysql_query("INSERT INTO cart(indice, idauto) VALUES (indice,$idauto)") or die('Query non riuscita'.mysql_error());
+				
+			//	$querypresdel = mysql_query("DELETE FROM cars WHERE id='$idauto'") or die('Query non riuscita'.mysql_error());
+		
+			//	$querypres = mysql_query("INSERT cart SELECT idauto FROM cart") or die('Query non riuscita'.mysql_error());
+                                // $querypres = mysql_query("SELECT * FROM carrello WHERE id='".$idauto."'") or die('Query non riuscita'.mysql_error());
+                               // if(mysql_num_rows($querypres))
+                              //  {
+                              //      $query = "DELETE FROM cart WHERE id='".$idauto."'";
+                              //      $result = mysql_query($query);
+                              //  }
+                            }
+                            
+                            if(isset($_GET["rimuovi"]) && ($_GET["rimuovi"]!=0))
+                            {
+                                $idauto = $_GET["rimuovi"];
+				$querypres = mysql_query("DELETE FROM cart WHERE idauto='$idauto'") or die('Query non riuscita'.mysql_error());
+			//	$querypres = mysql_query("INSERT INTO cart(indice, idauto) VALUES (indice,$idauto)") or die('Query non riuscita'.mysql_error());
+		
+			//	$querypres = mysql_query("INSERT cart SELECT idauto FROM cart") or die('Query non riuscita'.mysql_error());
+                                // $querypres = mysql_query("SELECT * FROM carrello WHERE id='".$idauto."'") or die('Query non riuscita'.mysql_error());
+                               // if(mysql_num_rows($querypres))
+                              //  {
+                              //      $query = "DELETE FROM cart WHERE id='".$idauto."'";
+                              //      $result = mysql_query($query);
+                              //  }
+                            }
+			?>
+
+                
+                
 	<br>
 	<br>
                 
