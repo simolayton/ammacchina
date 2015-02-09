1758b7d4ba47bdc747df96c9ceecb13bc070b596
simolayton <smameli13@gmail.com>
2015-02-09 12:30:18 +0100

    Update ricerca.php

--- php/cliente/ricerca.php
+++ php/cliente/ricerca.php
@@ -41,12 +41,83 @@
 		    
 		    
                 </header>
+                
+                <?php
+                $connessione_al_server = mysql_connect("localhost","mameliSimone","macaco861");
+                
+                if(!$connessione_al_server)
+                {
+                	die("Errore: connessione non riuscita".mysql_error());
+            	}
+            	$db_selected = mysql_select_db("amm14_mameliSimone", $connessione_al_server);
+            	if(!$db_selected)
+            	{
+                	die("Errore: selezione del database errata ".mysql_error());
+            	}
+            	$queryvis = mysql_query("SELECT * FROM cars WHERE stato='Auto usata'") or die("query non riuscita".mysql_error());
+            	$row = mysql_fetch_object($queryvis);
+                if(isset($_GET["ricercaauto"]))
+		{
+                            ?>
+                                <h3>Risultati:</h3>
+                            <?
+                                $_SESSION["marca"] = $_POST["marca"];
+                                $_SESSION["modello"] = $_POST["modello"];
+                                $_SESSION["anno"] = $_POST["anno"];
+                                $_SESSION["alimentazione"] = $_POST["alimentazione"];
+                                $_SESSION["prezzo"] = $_POST["prezzo"];
+                                $_SESSION["chilometri"] = $_POST["chilometri"];
+                                $wadd = "WHERE stato ='Auto usata'";
+                                if($_SESSION["marca"] !="")
+                                    $wadd .= " AND marca ='".$_SESSION["marca"]."'";
+                                if($_SESSION["modello"] !="")
+                                    $wadd .= " AND modello ='".$_SESSION["modello"]."'";
+                                if($_SESSION["anno"] !="")
+                                    $wadd .= " AND anno >='".$_SESSION["anno"]."'";
+                                if($_SESSION["alimentazione"] !="")
+                                    $wadd .= " AND alimentazione ='".$_SESSION["alimentazione"]."'";
+                                if($_SESSION["prezzo"] !="")
+                                    $wadd .= " AND prezzo <='".$_SESSION["prezzo"]."'";
+                                if($_SESSION["chilometri"] !="")
+                                    $wadd .= " AND chilometri <='".$_SESSION["chilometri"]."'";
+                                $queryvis = mysql_query("SELECT * FROM cars $wadd") or die("query non riuscita".mysql_error());
+                                if(mysql_num_rows($queryvis)==0)
+                                {
+                                ?>
+                                    <br><br><p>Nessun risultato.</p>
+                                <?
+                                }
+                                
+                                
+                                else
+                                {
+                                	?>
+                                	<br><br><p>Auto trovata! Clicca su PARCO AUTO</p>
+                                	<?
+                                }
+                                }
+                                
+                                ?>
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
                             
 		<div style="text-align: center">
 		
 		<h3>Ricerca la tua auto:</h3>
 		
-		<form action="home.php?ricercaauto" method="post" id="form-login">
+		<form action="ricerca.php?ricercaauto" method="post" id="form-login">
                 
                 <br>Marca: <input type="text" name="marca" placeholder="Tutto"/><br>
  
