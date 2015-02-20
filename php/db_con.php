850fcecb2dacd7ecd7f879620eeda35bfc87158f
simolayton <smameli13@gmail.com>
2015-02-20 12:12:17 +0100

    Update db_con.php

--- php/db_con.php
+++ php/db_con.php
@@ -1,19 +1,16 @@
 <?php
 
-//connessione al nostro database
 $connessione_al_server=mysql_connect("localhost","mameliSimone","macaco861");
-// ip locale, login e password
+
 if(!$connessione_al_server)
 {
-	// questo apparirà solo se ci sarà un errore
 	die ('Non riesco a connettermi: errore '.mysql_error());
 }
 
 $db_selected=mysql_select_db("amm14_mameliSimone",$connessione_al_server);
-// dove io ho scritto "prova" andrà inserito il nome del db
+
 if(!$db_selected)
 {
-	// se la connessione non andrà a buon fine apparirà questo messaggio
 	die ('Errore nella selezione del database: errore '.mysql_error()); 
 }
 
