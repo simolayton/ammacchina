e2b07dca45501a0f9efc7ca03e5462431071316e
simolayton <smameli13@gmail.com>
2015-02-21 12:21:30 +0100

    Update ricerca.php

--- php/dipendente/ricerca.php
+++ php/dipendente/ricerca.php
@@ -104,8 +104,8 @@
                 	<br>Modello:<input type="text" name="modello"><br>
                 	<br>Anno:<input type="number" name="anno" min="1930" max="2015"><br>
                 	<br>Alimentazione:
-                	                            <input type="radio" name="alimentazione" value="Benzina">Benzina
-                	                            <input type="radio" name="alimentazione" value="Diesel" checked>Diesel
+                	                            <input type="radio" name="alimentazione" value="Benzina" checked>Benzina
+                	                            <input type="radio" name="alimentazione" value="Diesel">Diesel
                 	                            <input type="radio" name="alimentazione" value="Gas">Gas
                 	                            <input type="radio" name="alimentazione" value="Elettrica">Elettrica
 	
